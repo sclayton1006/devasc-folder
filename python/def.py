22f0c7df93c2ad4e9ae2699581ffbcfc9ec5933d
sclayton1006 <sclayton1006@outlook.com>
2020-11-13 21:03:39 +0000

    Finalised function lesson code - 13th Nov 20

--- python/def.py
+++ python/def.py
@@ -1,7 +1,13 @@
-#Python
+# Python3.8
+
+# The whole point of this exercise is to demnstrate the capabilities of
+# a function. The code is simple to make the point of the lesson simpler
+# to understand.
+
+# Simon Clayton - 13th November 2020
 
 def subnet_binary(s):
-    """ Takes a slash notation subnet and calculates the 
+    """ Takes a slash notation subnet and calculates the
     number of host addresses """
     if s[0] == "/":
         a = int(s.strip("/"))
