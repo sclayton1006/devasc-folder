0589f148214339b1f4d10fd52481be229976df99
sclayton1006 <sclayton1006@outlook.com>
2020-11-13 20:52:53 +0000

    Updated def.py with the right syntax

--- python/def.py
+++ python/def.py
@@ -1,5 +1,4 @@
 #Python
-#Python
 
 def subnet_binary(s):
     """ Takes a slash notation subnet and calculates the 
@@ -9,6 +8,7 @@ def subnet_binary(s):
     else:
         a = int(s)
         addresses = (2**(32-a))
+    
     return addresses
 
 s = input("Enter your slash notation. Use a number or a slash (/27 or 27): ")
