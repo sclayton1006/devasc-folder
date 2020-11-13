9ce83c3562c9b3bd03acc4abaef7dec50be97e49
sclayton1006 <sclayton1006@outlook.com>
2020-11-13 20:43:06 +0000

    Second commit to remedy corrupt git file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to my README.md
+This is my public (but private) cloud repo for learning all about the different features in the DevAsc course. 
+> I like markdown. 
---
+++ python/def.py
@@ -0,0 +1,16 @@
+#Python
+#Python
+
+def subnet_binary(s):
+    """ Takes a slash notation subnet and calculates the 
+    number of host addresses """
+    if s[0] == "/":
+        a = int(s.strip("/"))
+    else:
+        a = int(s)
+        addresses = (2**(32-a))
+    return addresses
+
+s = input("Enter your slash notation. Use a number or a slash (/27 or 27): ")
+x = (subnet_binary(s))
+print(x)
