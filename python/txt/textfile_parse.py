7321a010bf930f0eb3b7126174a1969a69a2d677
Simon <sclayton1006@outlook.com>
2020-11-18 08:22:50 +0000

    Setting up Surface to act as a remote repo to continue studying 18112020 0822

--- python/txt/textfile_parse.py
+++ python/txt/textfile_parse.py
@@ -24,7 +24,7 @@ second argument in place of the 'r' that denotes 'read' and is the default """
 # "with open("txtfile.txt") as data:" if you want to read only. No 'r' required.
 
 with open("textfile.txt", "a+") as data:
-    data.write("I added this onto the end of the file")
+    data.write("I added this onto the end of the file\n")
 
 # Writing data into the file can be done with the above two lines but the same
 # line cannot be used to print the file. It didn't work for me anyway. I had
