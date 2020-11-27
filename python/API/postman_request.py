07fe2646d8c94f46a49d004bedfb1f3d683862d4
sclayton1006 <sclayton1006@outlook.com>
2020-11-27 10:46:22 +0000

    Updated the postman_request.py document to show some comfort messages and ASCII art 1046 27112020

--- python/API/postman_request.py
+++ python/API/postman_request.py
@@ -6,20 +6,35 @@
 
 # First import requests
 import requests
-import json
+import time
 
 # Then define the URL using a string
 url = "https://postman-echo.com/get"
-
+method = "GET"
 # Define your string for querying the server as a dictionary
 querystring = {"test":"123"}
 
 # Headers are blank for this example
 headers = {}
 
+# Comfort messaging, letting the user know something is happeneing. In
+# production this would not have a 1 second wait in it.
+print(f"Sending {method}. Please wait...\n\n")
+time.sleep(1)
+
 # Response will equal the HTTP get
-response = requests.request ("GET", url, headers=headers, params=querystring)
+response = requests.request (method, url, headers=headers, params=querystring)
 # Which i think becomes:
 # GET https://postman-echo.com/get?test=123
-
-print(response.text)
+if response != "":
+    # A bit of ASCII art for kicks...
+    print("                                \n\
+,---.                              |       \n\
+`---..   .,---.,---.,---.,---.,---.|       \n\
+    ||   ||    |    |---'`---.`---.        \n\
+`---'`---'`---'`---'`---'`---'`---'o       \n\
+                                    ")
+    print(f"The {method} request was successful. The results are below:\n\n\
+{response.text}")
+else:
+    print("API error. There is no data in the variable.")
