03769711c99578b48305b089fa446347b28162b8
sclayton1006 <sclayton1006@outlook.com>
2020-12-07 20:31:09 +0000

    Update

--- python/API/meraki/deviceList.py
+++ python/API/meraki/deviceList.py
@@ -3,16 +3,15 @@ import requests
 import json
 
 ident = orgId()
-url = (f"https://n149.meraki.com/api/v1/organizations/{ident}/networks")
+url = (f"https://n149.meraki.com/api/v0/organizations/{ident}/networks")
 headers = {'X-Cisco-Meraki-API-Key': "6bec40cf957de430a6f1f2baa056b99a4fac9ea0",
            'Accept': "application/json"}
 
 apiCall = requests.request("GET", url, headers=headers)
-apiClean = json.loads(apiCall.content)
-#print(apiClean)
+apiClean = json.loads(apiCall.text)
 
-for net in apiClean:
-    if net['organizationID'] == ident:
-        print(f"The network ID is: {net['id']}")
-
-#print(apiCall.text)
+#print(apiCall.json())
+n = 0
+for element in apiClean:
+    print (element['id'])
+    n += 1
