a021b25e4e514a56d8773de558b37203487996b3
sclayton1006 <sclayton1006@outlook.com>
2020-12-07 18:55:11 +0000

    Further updates

--- python/API/meraki/MerakiDevNetID.py
+++ python/API/meraki/MerakiDevNetID.py
@@ -1,8 +1,13 @@
-""" This is the big one, my own API to Meraki, which will pull information
-about devices. I will use rich tables to make this a little sexier on the
-eye... """
-
 def getMerakiSandboxId():
+
+    """ Imports the DevNet organization ID from the DevNet Sandbox. Import the
+    function using:
+
+        $ 'from MerakiDevNetID import getMerakiSandboxId as ID'
+
+    I've tried to make the script self-sufficient and it should just work for as
+    long as the sandbox is operational - SC """
+
     from meraki_sdk.meraki_sdk_client import MerakiSdkClient
 
     key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
@@ -15,6 +20,18 @@ def getMerakiSandboxId():
     return sBox
 
 def main():
+
+    """ Imports the DevNet organization ID from the DevNet Sandbox.
+
+    I've tried to make the script self-sufficient and it should just work for as
+    long as the sandbox is operational.
+
+        key = 6bec40cf957de430a6f1f2baa056b99a4fac9ea0
+        meraki = MerakiSdkClient(key)
+        orgs = meraki.organizations.get_organizations()
+
+    SC"""
+
     from meraki_sdk.meraki_sdk_client import MerakiSdkClient
 
     key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
