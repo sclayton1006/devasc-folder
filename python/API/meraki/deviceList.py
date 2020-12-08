75ce5402b4d2770112101438c6812d640693f674
sclayton1006 <sclayton1006@outlook.com>
2020-12-08 19:26:25 +0000

    Final copy of Meraki Python API and list loop. Lessons to be learned here... 1924 08122020

--- python/API/meraki/deviceList.py
+++ python/API/meraki/deviceList.py
@@ -10,4 +10,8 @@ headers = {'X-Cisco-Meraki-API-Key': "6bec40cf957de430a6f1f2baa056b99a4fac9ea0",
 apiCall = requests.request("GET", url, headers=headers)
 apiClean = json.loads(apiCall.text)
 
-print("hello123")
+for a in apiClean:
+    x = a['id']
+    y = a['name']
+    print (f"Network ID is: {x} and the location is: {y}")
+
